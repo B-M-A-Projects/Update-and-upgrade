ac94397b3885e892de99b70335e8481b7422a1b0
BMA <martin.andersson.lkpg@gmail.com>
2023-01-04 21:48:04 +0100

    Moved influx db credentials to separate csv file

--- update_and_upgrade.py
+++ update_and_upgrade.py
@@ -1,6 +1,15 @@
 import re
 import subprocess
 from influxdb import InfluxDBClient
+import csv
+
+with open('pwd.csv') as file:
+    csv_reader = csv.reader(file, delimiter = ',')
+    
+    for row in csv_reader:
+        user = row[0]
+        pwd = row[1]
+        db = row[2]
 
 response = subprocess.Popen('sudo apt-get update -y && sudo apt-get upgrade -y', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 #print(response)
@@ -20,7 +29,7 @@ upgrade_data = [
     }
 ]
 #print(upgrade_data)
-client = InfluxDBClient('localhost', 8086, '********', '********', '********')
+client = InfluxDBClient('localhost', 8086, user, pwd, db)
 #print(client)
 client.write_points(upgrade_data)
 client.close()
